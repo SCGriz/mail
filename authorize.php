c57cff6209c56ddc689e9ff41971bd269ffe2313
sCorbettPHSX <sean.corbett@umconnect.umt.edu>
2016-07-05 14:21:54 -0600

    modified:   authorize.php 	modified:   oauth.php

--- authorize.php
+++ authorize.php
@@ -1,6 +1,10 @@
 <?php
   session_start();
+  require_once('oauth.php');
   $auth_code = $_GET['code'];
+  $redirectUri = 'http://localhost/php-tutorial/authorize.php';
+
+  $tokens = oAuthService::getTokenFromAuthCode($auth_code, $redirectUri);
 ?>
 
-<p>Auth code: <?php echo $auth_code ?></p>
+<p>Access Token: <?php echo $tokens['access_token'] ?></p>
--- oauth.php
+++ oauth.php
@@ -20,5 +20,78 @@
       error_log("Generated login URL: ".$loginUrl);
       return $loginUrl;
     }
+
+    public static function getTokenFromAuthCode($authCode, $redirectUri) {
+      // Build the form data to post to the OAuth2 token endpoint
+      $token_request_data = array(
+        "grant_type" => "authorization_code",
+        "code" => $authCode,
+        "redirect_uri" => $redirectUri,
+        "scope" => implode(" ", self::$scopes),
+        "client_id" => self::$clientId,
+        "client_secret" => self::$clientSecret
+      );
+
+      // Calling http_build_query is important to get the data
+      // formatted as expected.
+      $token_request_body = http_build_query($token_request_data);
+      error_log("Request body: ".$token_request_body);
+
+      $curl = curl_init(self::$authority.self::$tokenUrl);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $token_request_body);
+
+      $response = curl_exec($curl);
+      error_log("curl_exec done.");
+
+      $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+      error_log("Request returned status ".$httpCode);
+      if ($httpCode >= 400) {
+        return array('errorNumber' => $httpCode,
+                     'error' => 'Token request returned HTTP error '.$httpCode);
+      }
+
+      // Check error
+      $curl_errno = curl_errno($curl);
+      $curl_err = curl_error($curl);
+      if ($curl_errno) {
+        $msg = $curl_errno.": ".$curl_err;
+        error_log("CURL returned an error: ".$msg);
+        return array('errorNumber' => $curl_errno,
+                     'error' => $msg);
+      }
+
+      curl_close($curl);
+
+      // The response is a JSON payload, so decode it into
+      // an array.
+      $json_vals = json_decode($response, true);
+      error_log("TOKEN RESPONSE:");
+      foreach ($json_vals as $key=>$value) {
+        error_log("  ".$key.": ".$value);
+      }
+
+      return $json_vals;
+    }
+
+    public static function getUserEmailFromIdToken($idToken) {
+      error_log("ID TOKEN: ".$idToken);
+
+      // JWT is made of three parts, separated by a '.'
+      // First part is the header
+      // Second part is the token
+      // Third part is the signature
+      $token_parts = explode(".", $idToken);
+
+      // We care about the token
+      // URL decode first
+      $token = strtr($token_parts[1], "-_", "+/");
+      // Then base64 decode
+      $jwt = base64_decode($token);
+      // Finally parse it as JSON
+      $json_token = json_decode($jwt, true);
+      return $json_token['preferred_username'];
+    }
   }
 ?>
